a39faf674b930bdd6025a54751696ddd372c1aca
Esma Koçak <esma.kocaakk20@gmail.com>
2023-09-06 23:33:05 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ function App() {
     <main className="bg-zinc-800 h-screen ">
       <div className="container mx-auto justify-center py-6">
         <h1 className="text-4xl text-center text-zinc-100">Hello, world!</h1>
+          <h2 className="text-zinc-100 text-2xl">This is Blog Fusion.</h2>
       </div>
     </main>
   );
