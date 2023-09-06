fe817de28280edabe18dbe44aae3393ff85c9b3a
esmakocak <kerem.4555@gmail.com>
2023-09-06 23:08:00 +0300

    Project and repo created

---
+++ README.local.md
@@ -0,0 +1,10 @@
+# Next ui to add
+
+- skeleton
+- alerts
+- dark theme switch
+- date picker
+
+# Create account with email and password
+
+# /home page will include all posts from all users
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="bg-zinc-800 h-screen ">
+      <div className="container mx-auto justify-center py-6">
+        <h1 className="text-4xl text-center text-zinc-100">Hello, world!</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
